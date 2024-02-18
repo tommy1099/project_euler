4cca9405af2686f70461f1cf8bcecace48c2287b
tommy1099 <amir.agness.aa@gmail.com>
2024-02-18 23:18:22 +0330

    commenting stuff

--- 1_Multiples_of_3_or_5/sol.hs
+++ 1_Multiples_of_3_or_5/sol.hs
@@ -1,4 +1,6 @@
+--gives a list with elements of multiples of 5 or 3
 sol :: [Int] -> [Int]
 sol xs = [x | x <- xs, x `mod` 3 == 0 || x `mod` 5 == 0]
 main :: IO()
+--sum of the list
 main = print (sum (sol [1..999]))
--- 2_Even_Fibonacci_Numbers/sol.hs
+++ 2_Even_Fibonacci_Numbers/sol.hs
@@ -1,12 +1,12 @@
-
+--fibo
 fibo :: Int -> Int
 fibo 0 = 0
 fibo 1 = 1
 fibo x = fibo (x - 1) + fibo (x - 2)
-
+--a fibo list under value 4mill with map and takeWhile 
 fiboList :: (Ord a1, Num a1, Num a2, Enum a2) => (a2 -> a1) -> [a1]
 fiboList fibo' = takeWhile (<= 4000000) (map fibo' [1..])
-
+--sum of the even elements on the list
 sol :: (Integral a) => [a] -> a
 sol xs = sum [x | x <- xs, even x]
 
