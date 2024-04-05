d54300de4d693119fb65807d92de035b171acafb
tommy1099 <amir.agness.aa@gmail.com>
2024-04-05 17:31:36 +0330

    more practices done

--- 2_Even_Fibonacci_Numbers/sol.hs
+++ 2_Even_Fibonacci_Numbers/sol.hs
@@ -1,14 +1,29 @@
---fibo
-fibo :: Int -> Int
-fibo 0 = 0
-fibo 1 = 1
-fibo x = fibo (x - 1) + fibo (x - 2)
---a fibo list under value 4mill with map and takeWhile 
-fiboList :: (Ord a1, Num a1, Num a2, Enum a2) => (a2 -> a1) -> [a1]
-fiboList fibo' = takeWhile (<= 4000000) (map fibo' [1..])
---sum of the even elements on the list
-sol :: (Integral a) => [a] -> a
-sol xs = sum [x | x <- xs, even x]
+-- --fibo
+-- fibo :: Int -> Int
+-- fibo 0 = 0
+-- fibo 1 = 1
+-- fibo x = fibo (x - 1) + fibo (x - 2)
+-- --a fibo list under value 4mill with map and takeWhile 
+-- fiboList :: (Ord a1, Num a1, Num a2, Enum a2) => (a2 -> a1) -> [a1]
+-- fiboList fibo' = takeWhile (<= 4000000) (map fibo' [1..])
+-- --sum of the even elements on the list
+-- sol :: (Integral a) => [a] -> a
+-- sol xs = sum [x | x <- xs, even x]
 
-main :: IO()
-main = print (sol (fiboList fibo))
+-- main :: IO()
+-- main = print (sol (fiboList fibo))
+
+-- Define an infinite list of Fibonacci numbers
+fibs :: [Integer]
+fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
+-- Filter even Fibonacci numbers below a limit
+evenFibsBelow :: Integer -> [Integer]
+evenFibsBelow limit = filter even $ takeWhile (< limit) fibs
+
+-- Sum of even Fibonacci numbers below a limit
+sumEvenFibsBelow :: Integer -> Integer
+sumEvenFibsBelow limit = sum $ evenFibsBelow limit
+
+main :: IO ()
+main = print fibs
--- 3_Largest_Prime_Factor/sol.hs
+++ 3_Largest_Prime_Factor/sol.hs
@@ -1,2 +1,7 @@
-main :: IO()
-main = print 5
+sol :: (Integral a) => a -> [a]
+sol num = tail [x | x <- [1 .. num ^ 2], num `mod` x == 0, [y | y <- [1 .. x], x `mod` y == 0] == [1, x]]
+
+-- primes a = [x | x <- [1 .. a], a `mod` x == 0, factors x == [1, x]]
+
+main :: IO ()
+main = print (sol 600851475143)
